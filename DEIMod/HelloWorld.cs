5e266ad74a70024dbe9e9fa9eedd528574343c0e
Janelle Cueto <janellecueto@gmail.com>
2019-02-05 10:29:00 -0600

    Added files via file upload

---
+++ DEIMod/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+namespace DEIMod
+{
+
+    //This command displays "Hello World!" in a pop-up dialogue box
+
+    [Transaction(TransactionMode.ReadOnly)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                TaskDialog.Show("My Dialog Title", "Hello World!");
+            }
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
